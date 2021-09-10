fab0718e9e31c1b40b4f27efb284e1b7ad585c50
marcoshaa <m.andrade.assis@gmail.com>
2021-09-09 23:34:59 -0300

    atualização da classe prog

--- src/aplication/prog.java
+++ src/aplication/prog.java
@@ -1,9 +1,34 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.enums.*;
+import entities.*;
+
 public class prog {
 	
 	public static void main (String []args) {
 		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.print("Entre com o nome do departamento: ");
+		String depName = sc.nextLine();
+		System.out.print("Entre com os dados do trabalho:");
+		System.out.print("Name: ");
+		String wName = sc.nextLine();
+		System.out.print("Nivel: ");
+		String wLevel = sc.nextLine();
+		System.out.print("Salario base: ");
+		Double bSalary = sc.nextDouble();
+		
+		work work = new work(wName, workLevel.valueOf(wLevel), bSalary, new depart(depName));
+		
+		
+		System.out.print("Qauntos contratos este trabalhador tem ?");
+		int n = sc.nextInt();
+				
+		sc.close();
 	}
 
 }
